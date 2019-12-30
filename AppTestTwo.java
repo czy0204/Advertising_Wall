7adb40f783568e78a43d37976916a97697d79988
hyh-star <59043926+hyh-star@users.noreply.github.com>
2019-12-30 13:45:01 +0800

    Update AppTestTwo.java

--- AppTestTwo.java
+++ AppTestTwo.java
@@ -4,26 +4,26 @@ import java.io.File;
 public class AppTestTwo {
    public static void main(String []args) {
       Login login = new Login();
-      login.setID("����");
+      login.setID("王林");
       login.setPassword("ilovethisgame");
       HandleLogin  handleLogin = new HandleLogin();
       login = handleLogin.handleLogin(login);
       if(login.getLoginSuccess()==false){
-         System.out.println("��¼ʧ��");
+         System.out.println("登陆失败");
          return;
       }
       else {
-         System.out.println("��¼�ɹ������Է������");
+         System.out.println("登陆成功，可以发布广告");
       }
       AddAdvertisement  add = new AddAdvertisement();
       add.setLogin(login);
       Advertisement ad = new Advertisement();
-      ad.setContent("�����Ļ���");
+      ad.setContent("美丽的花都");
       File f = new File("flow.jpg");
       ad.setPictureFile(f);
       ad.setSerialNumber("AR008");
       add.addAdvertisement(ad);
-      ad.setContent("��������");
+      ad.setContent("美丽旅游");
       f = new File("water.jpg");  
       ad.setPictureFile(f);
       ad.setSerialNumber("YA007");
