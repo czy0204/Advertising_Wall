408c874351da30ab4ef4aec1d28a006a13cb5fce
lyt1352555726 <59073130+lyt1352555726@users.noreply.github.com>
2019-12-30 14:20:02 +0800

    Add files via upload

---
+++ HandleQueryAllUserAD.java
@@ -0,0 +1,29 @@
+package ch4.view;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import ch4.data.*;
+public class HandleQueryAllUserAD implements ActionListener {
+   QueryAllUserAD  queryAll;
+   QueryAllUserADView view ;
+   public HandleQueryAllUserAD(){
+      queryAll = new QueryAllUserAD();
+   }
+   public void actionPerformed(ActionEvent e) {
+      queryAll.setLogin(view.login);
+      if(view.login.getLoginSuccess() == false){
+        JOptionPane.showMessageDialog
+        (null,"请先登录","消息对话框", JOptionPane.WARNING_MESSAGE);
+         return;
+      }
+      Advertisement [] ad = queryAll.queryAllUser();
+      if(ad == null ) return;
+      AdvertisingBoard board = new AdvertisingBoard(); 
+      board.setAdvertisement(ad);                     
+      view.pCenter.setAdvertisingBoard(board);        
+      view.pCenter.next.doClick();
+   }
+   public void setView(QueryAllUserADView view) {
+      this.view = view;
+   }
+}
