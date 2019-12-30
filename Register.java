dc2f15f7f757fcc9c8826d7fe73f6ce3f7e2aa59
CzyIsMyson <59045798+CzyIsMyson@users.noreply.github.com>
2019-12-30 13:45:46 +0800

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,17 @@
+package ch4.data;
+public class Register {
+   String id;
+   String password;
+   public void setID(String id){
+      this.id = id;
+   }
+   public void setPassword(String password){
+      this.password = password;
+   }
+   public String getID() {
+      return id;
+   }
+   public String getPassword(){
+      return password;
+   }
+} 
