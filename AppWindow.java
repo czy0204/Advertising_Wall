b55c66c6a2a7f7bff4bca7157e64cad62cd6018f
czy0204 <59044604+czy0204@users.noreply.github.com>
2019-12-31 16:14:59 +0800

    Delete AppWindow.java

--- AppWindow.java
+++
@@ -1,7 +0,0 @@
-package ch4.gui;
-import ch4.view.IntegrationView;
-public class AppWindow {
-   public static void main(String [] args) {
-      IntegrationView win = new IntegrationView(); 
-   }
-}
